df9298a6b59e8beaf70fa843eb2ddbca9306f88b
Elena Murphy <emurphy12@luc.edu>
2017-10-17 02:09:55 +0000

    finishing TODOs
    Signed-off-by: Elena Murphy <emurphy12@luc.edu>

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -1,60 +1,67 @@
 package edu.luc.cs271.linkedstack;
 
-import java.util.List;
+import java.util.*;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class LinkedStack<E> implements IStack<E> {
 
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
+
   private int n = 0;
 
   // TODO why don't we need an explicit constructor?
 
   @Override
   public E push(final E obj) {
-    // TODO
-    Node<E> old = top;
-    top = new Node<E>();
-    top.data = data;
-    top.next = old;
+    // DONE
+    top = new Node<E>(obj, top);
     n++;
+    return obj;
   }
 
   @Override
   public E peek() {
-    // TODO
+    // DONE
     if (isEmpty()) {
-      throw new NoSuchElementException("Stack underflow");
+      throw new NoSuchElementException();
     }
-    return top.item;
+    return top.data;
   }
 
   @Override
   public E pop() {
-    // TODO
-    if (isEmpty()) { 
-      throw new NoSuchElementException("Stack underflow");
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
     }
-    Item item = top.item;
+    Node<E> newTop = top;
     top = top.next;
     n--;
-    return item;
+    return newTop.data;
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return top == null;
+    // DONE
+    if (top == null) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   @Override
   public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    // TODO add any instance variable(s) required to support this
-    ArrayList<E> list = new ArrayList<E>(n);
-    
-    return null;
+    // DONE implement using an ArrayList preallocated with the right size
+    // DONE add any instance variable(s) required to support this
+    List<E> list = new ArrayList<E>(n);
+    while (top != null) {
+      list.add(top.data);
+      top = top.next;
+    }
+
+    return list;
   }
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -4,32 +4,24 @@ import java.util.Scanner;
 
 public class ReverseLines {
 
-	public static void main(String[] args) {
-		// TODO read successive input lines until EOF, then print out in reverse order
+  public static void main(String[] args) {
+    // DONE read successive input lines until EOF, then print out in reverse order
 
-	    final Scanner input = new Scanner(System.in);
-	    int n = 0;
-	    
-	    LinkedStack<String> stack = new LinkedStack<String>();
-	    
-		String line;
-		while ((line = input.nextLine()) != null) {
-			
-			line = input.nextLine();
-			if(line == "EOF"){
-				break;
-			}
-			else{
-				stack.push(line);
-				n++;
-			}
-		
-		}
+    LinkedStack<String> stack = new LinkedStack<String>();
+    String line;
+    final Scanner input = new Scanner(System.in);
 
-		for(int i = 0; i < stack.asList().size(); i++){
-			
-			System.out.println(stack.asList());
-		}
+    while (input.hasNextLine()) {
 
-	}
+      line = input.nextLine();
+      stack.push(line);
+      if (line == "") {
+        break;
+      }
+    }
+
+    while (!stack.isEmpty()) {
+      System.out.println(stack.pop());
+    }
+  }
 }
